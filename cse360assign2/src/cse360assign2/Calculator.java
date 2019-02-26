aedadbe100bb5ca70e1e312ac02bffc96c47e2df
oliviachan <oliviachan@livchan.3520.dhcp.asu.edu>
2019-02-25 23:13:42 -0700

    Initial Version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,46 @@
+// Name: Olivia Chan
+// Class ID: 347
+// Assignment #2
+
+/**
+ * Initial version contains constructor and empty methods for basic mathematical 
+ * operations.
+ * 
+ * @author Olivia Chan
+ * 
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
